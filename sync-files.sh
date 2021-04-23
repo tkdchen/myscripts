6e3620c19acbdbf375141b21f6b19daab780fe50
Chenxiong Qi <qcxhome@gmail.com>
2021-04-23 16:58:12 +0800

    Update sync-files.sh
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- sync-files.sh
+++ sync-files.sh
@@ -3,15 +3,33 @@
 set -ex
 
 src_host=$1
-src_home=$2
+remote_user=${2:-$USER}
+src_home="/home/${remote_user}"
+src_root="${remote_user}@${src_host}:${src_home}"
 
-dirs=(Documents Music Pictures Videos books certificates irclogs .ssh)
+personal_data() {
+dirs=(Documents Music Pictures Videos books certificates irclogs)
 
 for src_dir in "${dirs[@]}"; do
-  rsync -arv ${src_host}:${src_home}/${src_dir} $HOME
+  rsync -arv ${src_root}/${src_dir} $HOME
 done
+}
 
 # TODO: sync up other necessary configuration files
 
-rsync -av ${src_host}:${src_home}/.pypirc $HOME
-rsync -av ${src_host}:${src_home}/.config/pip/pip.conf $HOME/.config/pip/pip.conf
+sync_config_files() {
+rsync -av ${src_root}/.ssh/config $HOME/.ssh/
+rsync -av ${src_root}/.pypirc $HOME
+rsync -av ${src_root}/.emacs $HOME
+
+config_dir="$HOME/.config"
+pip_config_dir="${config_dir}/pip"
+[ -e "$pip_config_dir" ] || mkdir "$pip_config_dir"
+rsync -av ${src_root}/.config/pip/pip.conf "${pip_config_dir}/pip.conf"
+
+rsync -arv ${src_root}/.config/jenkins "$config_dir"
+rsync -arv ${src_root}/.config/jenkins_jobs "$config_dir"
+}
+
+#personal_data
+sync_config_files
